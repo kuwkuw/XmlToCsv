e0b2c065503dee53134f1adf82c574f78077ac8c
kuwkuw <kuw-kuw@mail.ru>
2015-08-18 14:16:40 +0300

    #1 initial commit

---
+++ XMLtoCSV/Converter.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace XMLtoCSV
+{
+    public class Converter
+    {
+        public void Convert(string path)
+        {
+            //Checking path to file exists.
+            if(!File.Exists(path))
+                throw new ApplicationException("File don`t exists");
+
+            XElement xElement = XElement.Load(path);
+
+            var rows = xElement.Elements().Elements("row");
+
+            var heade = rows.First().Elements().Select(i => i.Attribute("name").Value);
+            //Write items in to CSV file.
+            using (var file = File.Create(Path.GetFileNameWithoutExtension(path)+".csv"))
+            {
+                StreamWriter sw = new StreamWriter(file);
+                sw.WriteLine(ToStr(heade));
+                foreach (var item in rows)
+                {
+                    sw.WriteLine(ToStr(item.Elements().Select(i => i.Value)));
+                }
+            }
+        }
+        /// <summary>
+        /// Create CSV row.
+        /// </summary>
+        /// <param name="list">Elemets list</param>
+        /// <returns>CSV row(string)</returns>
+       private string ToStr(IEnumerable<string> list)
+        {
+            StringBuilder itemStringBuilder = new StringBuilder();
+
+            foreach (var item in list)
+            {
+                itemStringBuilder.Append(XmlItemConverToCsvItem(item) + ",");
+            }
+
+            return itemStringBuilder.ToString();
+        }
+
+        private string XmlItemConverToCsvItem(string xmlItem)
+        {
+            if (xmlItem.Split(' ').Count()>1 || xmlItem.Contains(','))
+                return String.Format("\"{0}\"",  xmlItem);
+            
+
+            if (xmlItem.Equals(String.Empty))
+                return "\"\"";
+            
+
+            return xmlItem;
+        }
+    }
+}
---
+++ XMLtoCSV/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XMLtoCSV
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Convert XML to CSV");
+            Converter converter = new Converter();
+            try
+            {
+                converter.Convert("FEHPackageLevelDetail.xml");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
